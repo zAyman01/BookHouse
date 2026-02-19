92ef88c510f3db5b77d6400d7122e5cd7e55db09
Mahmoud Ayman <2301115@student.eelu.edu.eg>
2026-02-19 16:55:59 +0200

    favorites page semi done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import Favorites from './pages/Favorites';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/library" element={<h1>Library Page</h1>} />
-        <Route path="/favorites" element={<h1>Favorites Page</h1>} />
+        <Route path="/favorites" element={<Favorites />} />
         <Route path="/orders" element={<h1>Orders Page</h1>} />
         <Route path="/account" element={<h1>Account Page</h1>} />
         <Route path="/book" element={<h1>Book Page</h1>} />
