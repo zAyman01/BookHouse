c949519743f2f1679c02a86c38b36698e4dbf48a
A7MEDBX <ahmedbod50@gmail.com>
2026-03-03 15:17:39 +0200

    my bad :)

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`✅ MongoDB Connected`);
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB Connection Error: ${error.message}`);
     process.exit(1);
